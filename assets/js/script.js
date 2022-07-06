9a88195a3958cb8a9172b991c906547d95196871
Steve Calla <callasteven@gmail.com>
2022-07-06 16:05:10 -0600

    Function to determine win or lose

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,43 +3,53 @@ let playGameButton = document.getElementById('playGame');
 let timerInfo = document.getElementById('timer-readout');
 let inputCharacter = document.getElementById('input-character');
 let guessStatus = document.getElementById('guess-status');
+let timeInGame = document.getElementById('timer-readout');
 
 //section:global variables go here 👇
+let count = 10;
+let win = [];
+let countDownTime;
 
 //section:event listeners go here 👇
 playGameButton.addEventListener('click', playGame);
 inputCharacter.addEventListener('input', getInput);
 
 //section:functions and event handlers go here 👇
+timeInGame.innerText = `Time in Game: ${count} second(s)`;
+
 function playGame() {
   console.log('Ready to play');
+  timeInGame.innerText = `Time in Game: ${count} second(s)`;
   gameTimer();
 }
 
 function gameTimer() {
-  let count = 10;
-  let timeInGame = setInterval(() => {
+  countDownTime = setInterval(() => {
       if (count > 0) {
-        console.log(count);
         count--;
-        timerInfo.innerText = "Time Remaining = " + count;
+        timeInGame.innerText = `Time in Game: ${count} second(s)`;
+        inputCharacter.removeAttribute('disabled', 'false');
       } else {
-        console.log(count)
-        clearInterval(timeInGame);
+        clearInterval(countDownTime);
         console.log('Game over');
+        guessStatus.innerText = "Game Over. Out of Time. You Lost!"
+        inputCharacter.setAttribute('disabled', 'true');
+        count = 10
+        // timeInGame.innerText = `Time in Game: ${count} seconds`;
       }
     }, 1000);
+
 }
 
 function getInput() {
   console.log(inputCharacter.value);
   let guess = inputCharacter.value;
-  //check input
   checkInput(guess);
 }
 
 function checkInput(guess) {
   let word = 'guess';
+
   for (let i = 0; i < word.length; i++) {
     guess === '' ? console.log(null) : guess;
 
@@ -52,11 +62,27 @@ function checkInput(guess) {
     }
 
     if (word.charAt(i) === guess) {
-      console.log('yes');
       document.querySelectorAll('li')[i].innerText = guess;
-    } else {
-      console.log('no');
+      win[i] = guess;
     }
+    console.log(win);
     // inputCharacter.value = null;
   }
+
+  determineWin(word, guess);
+}
+
+function determineWin(word, guess) {
+  console.log(win)
+  if (win.join('') === word) {
+    console.log('you win', win.length, word.length)
+    inputCharacter.setAttribute('disabled', 'true');
+    inputCharacter.value = null;
+    guessStatus.innerText = "You win with " + count + " second(s) to spare!!"
+    clearInterval(countDownTime);
+    // timeInGame.innerText = `Time in Game: ${count} seconds`;
+    count = 10;
+    return;
+  } 
+
 }
