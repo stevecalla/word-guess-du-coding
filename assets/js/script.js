1d3a1d35c00ed99354efa3ab3c217bdb24c91825
Steve Calla <callasteven@gmail.com>
2022-07-06 14:08:34 -0600

    Add timer interval function and display in window

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 //section:query selector variables go here 👇
 let playGameButton = document.getElementById('playGame');
+let timerInfo = document.getElementById('timer-readout')
 
 //section:global variables go here 👇
 
@@ -9,4 +10,20 @@ playGameButton.addEventListener('click', playGame);
 //section:functions and event handlers go here 👇
 function playGame() {
   console.log('Ready to play');
+  gameTimer();
+}
+
+function gameTimer() {
+  let count = 10;
+  let timeInGame = setInterval(() => {
+      if (count > 0) {
+        console.log(count);
+        count--;
+        timerInfo.innerText = "Time Remaining = " + count;
+      } else {
+        console.log(count)
+        clearInterval(timeInGame);
+        console.log('Game over');
+      }
+    }, 1000);
 }
